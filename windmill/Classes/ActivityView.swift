qnoid/windmill-osx

commit b9428618e20b77c3ebf97baef5478a4f7c083b34

Added export and deploy stages
Now animating each stage to communicate progress of each activity; fixed #11

windmill/Classes/ActivityView.swift
@@ -38,5 +38,21 @@ class ActivityView: NSView {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         wml_addSubview(view: wml_load(view: ActivityView.self)!, layout: .centered)
-    }    
+    }
+    
+    func animations(activityType: ActivityType) -> [NSImage] {
+        let type = String(describing: activityType.rawValue)
+        
+        return [NSImage(named: "lights-\(type)-animation-key-1")!, NSImage(named: "lights-\(type)-animation-key-2")!, NSImage(named: "lights-\(type)-animation-key-3")!]
+    }
+
+    func startLightsAnimation(activityType: ActivityType) {
+        let animations = self.animations(activityType: activityType)
+        
+        self.imageView.layer?.add(CAAnimation.Windmill.lightsAnimation(size: self.imageView.bounds.size, animations: animations), forKey: "lights")
+    }
+    
+    func stopLightsAnimation() {
+        self.imageView.layer?.removeAnimation(forKey: "lights")
+    }
 }

commit 55ef91c8cc566d5392e6647c093c2ebda4b564c9

! fixed broken* animation introduced in b9428618e20b77c3ebf97baef5478a4f7c083b34 * broken since macOS High Sierra, (up until 10.13.2)

windmill/Classes/ActivityView.swift
@@ -12,7 +12,12 @@ import AppKit
 class ActivityView: NSView {
 
     @IBOutlet weak var titleLabel: NSTextField!
-    @IBOutlet weak var imageView: NSImageView!
+    @IBOutlet weak var imageView: NSImageView! {
+        didSet{
+            imageView.layer = CALayer()
+            imageView.wantsLayer = true
+        }
+    }
     
     @IBInspectable var title: String? {
         didSet{
@@ -49,10 +54,10 @@ class ActivityView: NSView {
     func startLightsAnimation(activityType: ActivityType) {
         let animations = self.animations(activityType: activityType)
         
-        self.imageView.layer?.add(CAAnimation.Windmill.lightsAnimation(size: self.imageView.bounds.size, animations: animations), forKey: "lights")
+        self.imageView.layer?.sublayers?[0].add(CAAnimation.Windmill.lightsAnimation(size: self.imageView.bounds.size, animations: animations), forKey: "lights")
     }
     
     func stopLightsAnimation() {
-        self.imageView.layer?.removeAnimation(forKey: "lights")
+        self.imageView.layer?.sublayers?[0].removeAnimation(forKey: "lights")
     }
 }